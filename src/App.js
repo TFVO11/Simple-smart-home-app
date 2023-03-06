cf825424ae6c27b517eac2385da35d51b0734be6
dogyun-wildseed <dogyun@wildseed.io>
2023-03-06 21:48:24 +0900

    refactor power nav

--- src/App.js
+++ src/App.js
@@ -4,26 +4,28 @@ import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Header from "./components/Header/Header";
 import KitchenContentsBox from "./components/contents/KitchenContentsBox";
 import PowerContentsBox from "./components/contents/PowerContentsBox";
-import KitchenNav from "./components/Navigation/KitchenNav";
-import PowerNav from "./components/Navigation/PowerNav";
-import Card from "./components/UI/Card";
-
 
 const App = () => {
     return (
-<Fragment>
-    <div>
-        <Header />
-        </div>
-        <div>
-       <Switch>
-        <Route exact path='/KitchenContentsBox/*' component={KitchenContentsBox}></Route>
-        <Route path='/PowerContentsBox/*' component={PowerContentsBox}></Route>
-        </Switch>
-    </div>
-</Fragment>
-    )
-
+        <Fragment>
+            <div>
+                <Header />
+            </div>
+            <div>
+                <Switch>
+                    <Route
+                        exact
+                        path="/KitchenContentsBox/*"
+                        component={KitchenContentsBox}
+                    ></Route>
+                    <Route
+                        path="/PowerContentsBox/*"
+                        component={PowerContentsBox}
+                    ></Route>
+                </Switch>
+            </div>
+        </Fragment>
+    );
 };
 
 export default App;
